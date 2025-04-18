30d4c1d1528e4ab7ad1addadc959ef0d78616cda
Islam <Islam@DESKTOP-AFA007P>
2025-04-18 13:30:09 +0200

    Employee Services & Dto's 05

---
+++ IKEA.BLL/Services/EmployeeServices/IEmployeeServices.cs
@@ -0,0 +1,19 @@
+﻿using IKEA.BLL.Dto_s.Departments;
+using IKEA.BLL.Dto_s.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.BLL.Services.EmployeeServices
+{
+    public interface IEmployeeServices
+    {
+        IEnumerable<EmployeeDto> GetAllEmployee();
+        EmployeeDetailsDto? GetEmployeeById(int id);
+        int CreateEmployee(CreatedEmployeeDto employeeDto);
+        int UpdateEmployee(UpdatedEmployeeDto employeeDto);
+        bool DeleteEmployee(int id);
+    }
+}
